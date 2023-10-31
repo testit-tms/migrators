3b3c692470db34a3bff1adb2da3c642a57466426
Pavel Butuzov <pavel.butuzov@testit.software>
2023-10-31 14:26:10 +0300

    Added TestLinkExporter.

--- Migrators/TestLinkExporter/Services/StepService.cs
+++ Migrators/TestLinkExporter/Services/StepService.cs
@@ -32,56 +32,8 @@ public class StepService : IStepService
             }
         );
         }
-
-
-        //var steps = azureSteps.Steps.Select(ConvertStep).ToList();
-
-        //foreach (var sharedStep in azureSteps.SharedSteps)
-        //{
-        //    if (sharedStepMap.TryGetValue(sharedStep.Id, out var sharedStepId))
-        //    {
-        //        steps.Add(new Step
-        //            {
-        //                SharedStepId = sharedStepId,
-        //                ActionAttachments = new List<string>(),
-        //                ExpectedAttachments = new List<string>(),
-        //                TestDataAttachments = new List<string>(),
-        //                Action = string.Empty,
-        //                Expected = string.Empty
-        //            }
-        //        );
-        //    }
-        //    else
-        //    {
-        //        throw new ApplicationException("Shared step not found in map");
-        //    }
-
-        //    steps.AddRange(sharedStep.Steps.Select(ConvertStep).ToList());
-        //}
-
         _logger.LogDebug("Converted steps: {@Steps}", steps);
 
         return steps;
     }
-
-    //private static Step ConvertStep(AzureStep azureStep)
-    //{
-    //    return new Step
-    //    {
-    //        Action = azureStep.Values[0],
-    //        Expected = azureStep.Values[1],
-    //        ActionAttachments = new List<string>(),
-    //        ExpectedAttachments = new List<string>(),
-    //        TestDataAttachments = new List<string>(),
-    //    };
-    //}
-
-    //private static AzureSteps ParseStepsFromXmlContent(string stepsContent)
-    //{
-    //    using TextReader stepsReader = new StringReader(stepsContent);
-    //    var serializer = new XmlSerializer(typeof(AzureSteps));
-    //    var azureSteps = (AzureSteps)serializer.Deserialize(stepsReader)!;
-
-    //    return azureSteps;
-    //}
 }
